4503fb323a82160960926b197051536fa9f1c148
Carol Phillips Hatter <carol@Carols-MacBook-Pro.local>
2020-07-21 11:01:24 -0700

    Add regression and hide api token

--- bookworm/core.py
+++ bookworm/core.py
@@ -1,17 +1,19 @@
 from sodapy import Socrata
 
+
 def create_client(app_token=None, timeout=180):
     """
-    Returns a Socrata client to query data.seattle.gov and resets the
+    Return a Socrata client to query data.seattle.gov and reset the
     timeout to 180 seconds if nothing is specified
     """
     client = Socrata("data.seattle.gov", app_token)
-    client.timeout=timeout
+    client.timeout = timeout
     return client
 
 
 def query_checkout_data(client, query):
     """
-    Takes a data.seattle.gov Socrata client and a SOQL
+    Take a data.seattle.gov Socrata client and a SOQL query and return
+    a pandas dataframe with the appropriate data.
     """
     return client.get('tmmm-ytt6', query=query)
